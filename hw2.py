1cffe47b65a7c95c8698547542e79b6d7c8f90b1
jaesnny <jchu8@cougarnet.uh.edu>
2023-02-22 21:02:15 -0600

    post method

--- hw2.py
+++ hw2.py
@@ -3,6 +3,13 @@ from flask import jsonify
 from flask import request
 import mysql.connector
 
+"""
+References:
+Homework 1
+Class 5 restapi.py
+https://pythonbasics.org/flask-http-methods/
+"""
+
 
 # setting up an application name
 app = flask.Flask(__name__) # sets up application
@@ -21,13 +28,36 @@ mycursor = snowboard_db.cursor()
 # get all snowboards from table: 
 @app.route('/api/snowboard/get', methods=['GET']) 
 def api_get_sb():
+    # SQL statement to post table
     return_table = "SELECT * FROM snowboard"
     mycursor.execute(return_table)
     rows = mycursor.fetchall()
+    # returns row
     return jsonify(rows)
-    
 
+
+
+# user add snowboard to table
+@app.route('/api/snowboard/add', methods=['POST'])
+def api_add_sb():
+    # request_data = request.get_json()
+    name = request.form['name']
+    boardtype = request.form['boardtype']
+    brand = request.form['brand']
+    msrp = request.form['msrp']
+    size = request.form['size']
     
+    # formats which column to add new items to 
+    row_add = """INSERT INTO snowboard (name, boardtype, brand, msrp, size) VALUES (%s, %s, %s, %s, %s)"""
+    row_value = (name, boardtype, brand, msrp, size)
+
+    mycursor.execute(row_add, row_value)
+    snowboard_db.commit()
+    #confirms to user that row was added
+    return 'Row added!'
+
+
+
 
 
 
